b0740a987be0f46f21df30ff61175d9362dee61f
ander-son-pi <apinzon02@misena.edu.co>
2024-09-24 13:17:16 -0500

    actualizar main.py hicimos una api con fastapi

--- web_server/main.py
+++ web_server/main.py
@@ -1,8 +1,33 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-def run():
-    #Llama a la funcion que solicita un GET a una api
+app = FastAPI()
+
+@app.get('/')
+def get_numbers():
+    """
+    Ruta principal que devuelve una lista de números.
+    """
+    return [1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def contact_page():
+    """
+    Ruta que devuelve una página HTML con un título y un párrafo.
+    """
+    return """
+        <h1>Hola, soy una página</h1>
+        <p>Soy un párrafo de ejemplo.</p>
+    """
+
+def fetch_categories():
+    """
+    Llama a la API de categorías desde el módulo 'store'.
+    """
+ 
     store.get_categories()
-    
+ 
+
 if __name__ == '__main__':
-    run()
+    fetch_categories()
